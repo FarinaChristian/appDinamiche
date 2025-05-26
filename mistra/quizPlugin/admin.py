55366f78de70c64aa9fece81a3fb8d7fa960b6d0
gfurri20 <geremia.furri@gmail.com>
2025-05-26 17:14:29 +0200

    features: fixtures and admin interface
    - added fixtures to load mockup data
    - edited admin panel for `TextExecution`
    - added Inline for `GivenAnswer`

--- mistra/quizPlugin/admin.py
+++ mistra/quizPlugin/admin.py
@@ -1,12 +1,114 @@
 from django.contrib import admin
 from .models import *
-
-# Register your models here.
+from django.utils.html import format_html
 
 admin.site.register(Category)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Test)
 admin.site.register(Sex)
-admin.site.register(TestExecution)
 admin.site.register(GivenAnswer)
+
+class GivenAnswerInline(admin.TabularInline):
+    model = GivenAnswer
+    
+    # Campi da mostrare nell'inline
+    fields = ('get_question_name', 'question_text_display', 'answer_text_display', 'formatted_score', 'answer_correction_display')
+    readonly_fields = fields
+
+    extra = 0
+    can_delete = False
+
+    verbose_name = "Risposta data"
+    verbose_name_plural = "Dettaglio Risposte Date e Punteggi"
+    
+    classes = ('collapse',)
+
+    # Metodi per mostrare i campi personalizzati
+    def get_question_name(self, obj):
+        return obj.answer.question.name
+    get_question_name.short_description = "Nome Domanda"
+
+    def question_text_display(self, obj):
+        text = obj.question_text
+        return (text[:100] + '...') if len(text) > 100 else text
+    question_text_display.short_description = "Testo Domanda"
+
+    def answer_text_display(self, obj):
+        text = obj.answer_text
+        return (text[:75] + '...') if len(text) > 75 else text
+    answer_text_display.short_description = "Testo Risposta Data"
+
+    def formatted_score(self, obj):
+        score_value = obj.answer_score_value
+        score_display = obj.answer_score_display
+        
+        color = "green"
+        if score_value == Answer.AnswerScore.ERROR:
+            color = "orange"
+        elif score_value == Answer.AnswerScore.SERIOUS_ERROR:
+            color = "red"
+        
+        return format_html('<span style="color: {}; font-weight: bold;">{}</span>', color, score_display)
+    
+    formatted_score.short_description = "Punteggio Risposta"
+    
+    def answer_correction_display(self, obj):
+        text = obj.answer_correction
+        if not text:
+            return "-"
+        return (text[:75] + '...') if len(text) > 75 else text
+    answer_correction_display.short_description = "Correzione"
+
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+    
+
+@admin.register(TestExecution)
+class TestExecutionAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'revision_code',
+        'age',
+        'sex',
+        'ip',
+        'execution_time',
+        'score',
+        'duration',
+        'revision_date',
+        'note',
+    )
+    
+    search_fields = ('revision_code', 'ip')
+    
+    list_filter = ('execution_time', 'revision_date')
+    
+    readonly_fields = ('revision_code', 'execution_time')
+    
+    fieldsets = (
+        ('Informazioni Utente', {
+            'fields': ('age', 'sex', 'ip'),
+            'classes': ('collapse',),
+        }),
+        ('Informazioni Esecuzione', {
+            'fields': ('execution_time', 'score', 'duration'),
+            'classes': ('collapse',),
+        }),
+        ('Revisione', {
+            'fields': ('revision_code', 'revision_date', 'note'),
+            'classes': ('collapse',),
+        })
+    )
+    
+    inlines = [GivenAnswerInline]
+
+    def get_queryset(self, request):
+        # Ottimizza le query per caricare i dati correlati
+        return super().get_queryset(request).prefetch_related(
+            'given_answers_through__answer__question', 
+            'test'
+        )
--- mistra/quizPlugin/models.py
+++ mistra/quizPlugin/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+import random
+import string
 
 
 class Category(models.Model):
@@ -22,16 +25,16 @@ class Question(models.Model):
         verbose_name_plural = "Domande"
 
     def __str__(self):
-        return self.name
+        return "{} - {}".format(self.name, self.category.name) 
 
 
 class Answer(models.Model):
     text = models.TextField()
     
     class AnswerScore(models.IntegerChoices):
-        SERIOUS_ERROR = -1
-        ERROR = 0
-        CORRECT = 1
+        SERIOUS_ERROR = -1, "Errore Grave"
+        ERROR = 0, "Errore"
+        CORRECT = 1, "Corretto"
     score = models.IntegerField(choices=AnswerScore)
     
     correction = models.TextField()
@@ -72,16 +75,18 @@ class Sex(models.Model):
 
 
 class TestExecution(models.Model):
-    execution_time = models.DateTimeField(auto_now_add=True)
-    age = models.IntegerField()
-    sex = models.ForeignKey(Sex, related_name='test_executions', on_delete=models.CASCADE)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='test_executions')
-    score = models.IntegerField()
-    ip = models.GenericIPAddressField()
-    duration = models.DurationField()
-    revision_date = models.DateTimeField(null=True, blank=True)
-    given_answer = models.ManyToManyField(Answer, related_name='test_executions')
-    note = models.TextField()
+    age = models.IntegerField(verbose_name="Età utente")
+    ip = models.GenericIPAddressField(verbose_name="Indirizzo IP utente")
+    execution_time = models.DateTimeField(auto_now_add=True, verbose_name="Data d'esecuzione")
+    score = models.IntegerField(verbose_name="Punteggio", default=0)
+    duration = models.DurationField(verbose_name="Durata")
+    revision_date = models.DateTimeField(null=True, blank=True, verbose_name="Data di revisione")
+    note = models.TextField(verbose_name="Note di revisione")
+    
+    sex = models.ForeignKey(Sex, related_name='test_executions', on_delete=models.CASCADE, verbose_name="Sesso utente")
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='test_executions', verbose_name="Test relativo")
+    
+    revision_code = models.CharField(max_length=12, unique=True, editable=False, blank=True, verbose_name="Codice di revisione")
     
     class Meta:
         verbose_name = "Esecuzione Test"
@@ -89,16 +94,63 @@ class TestExecution(models.Model):
         ordering = ['-execution_time']
         
     def __str__(self):
-        return "{}: {} - score {}".format(self.ip, self.execution_time, self.score)
+        return self.revision_code
+    
+    def _generate_unique_revision_code(self):
+        """Generates a unique revision code"""
+        if not self.execution_time:
+            exec_time = timezone.now().date()
+        else:
+            exec_time = self.execution_time
+            
+        date_str = exec_time.strftime("%Y%m%d")
+        
+        while True:
+            random_chars = "".join(random.choices(string.ascii_uppercase, k=3))
+            potential_code = f"{date_str}-{random_chars}"
+            
+            # check if it already exists
+            if not TestExecution.objects.filter(revision_code=potential_code).exists():
+                return potential_code
+            
+    def save(self, *args, **kwargs):
+        if not self.revision_code:
+            self.revision_code = self._generate_unique_revision_code()
+        
+        super().save(*args, **kwargs)
 
 
 class GivenAnswer(models.Model):
-    test_execution = models.ForeignKey(TestExecution, on_delete=models.CASCADE)
+    test_execution = models.ForeignKey(TestExecution, on_delete=models.CASCADE, related_name='given_answers_through')
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    
+
     class Meta:
         verbose_name = "Risposta Data"
         verbose_name_plural = "Risposte Date"
-        
+
     def __str__(self):
-        return self.id
+        if self.pk:
+            return f"Esecuzione: {self.test_execution.revision_code} - Domanda: {self.answer.question.name[:30]}... - Risposta: {self.answer.text[:30]}..."
+        return f"Nuova Risposta Data (ID: {self.id})"
+
+
+    # Proprietà per facilitare l'accesso nell'admin inline
+    @property
+    def question_text(self):
+        return self.answer.question.text
+
+    @property
+    def answer_text(self):
+        return self.answer.text
+
+    @property
+    def answer_score_value(self):
+        return self.answer.score
+
+    @property
+    def answer_score_display(self):
+        return self.answer.get_score_display()
+
+    @property
+    def answer_correction(self):
+        return self.answer.correction
