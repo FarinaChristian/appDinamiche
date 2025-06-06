646cc8fb2a324c1f60571a0d558909f4a6e7369c
Enrico Bragastini <enri.braga@gmail.com>
2025-06-06 11:20:17 +0200

    salvataggio sex, age; url separato per pagina test completato

--- mistra/quizPlugin/urls.py
+++ mistra/quizPlugin/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:test_id>/start/', views.start_test, name='quiz-question'),
     path('<int:test_id>/submit/', views.submit_answer, name='quiz-submit'),
+    path('completed/<str:revision_code>/', views.test_completed_view, name='quiz-completed'),
     path('', views.home, name='quiz-home'),
 ]
--- mistra/quizPlugin/views.py
+++ mistra/quizPlugin/views.py
@@ -6,28 +6,41 @@ from datetime import timedelta
 
 def home(request):
     tests = Test.objects.all()
-    return render(request, 'home.html', {'tests': tests})
+    sex = Sex.objects.all()
+
+    context = {
+        'tests': tests,
+        'sex': sex,
+    }
+
+    return render(request, 'home.html', context)
 
 
 def start_test(request, test_id):
+    # Recupera il test e le domande associate
     test = get_object_or_404(Test, id=test_id)
 
     question_list = list(test.questions.all())
     total_questions = len(question_list)
 
     if 'execution_id' not in request.session:
-        sex = Sex.objects.first()  # per semplicità
+        # Se non esiste un'esecuzione del test nella sessione, creane una nuova
+        age = request.POST.get('age')
+        sex = request.POST.get('sex')
+
         execution = TestExecution.objects.create(
-            age=25,
+            age=age,
+            sex=Sex.objects.get(name=sex),
             ip=request.META.get('REMOTE_ADDR'),
             test=test,
-            sex=sex,
             duration=timedelta(0)
         )
+
         request.session['execution_id'] = execution.id
         request.session['question_index'] = 0
         request.session['start_time'] = timezone.now().isoformat()
     else:
+        # Se esiste già un'esecuzione, recuperala
         execution = get_object_or_404(TestExecution, id=request.session['execution_id'])
 
     index = request.session.get('question_index', 0)
@@ -43,19 +56,29 @@ def start_test(request, test_id):
         for k in ['execution_id', 'question_index', 'start_time']:
             request.session.pop(k, None)
 
-        return render(request, 'test_completed.html', {
-            'execution': execution,
-            'given_answers': given_answers,
-        })
+        return redirect('quiz-completed', revision_code=execution.revision_code)
 
     current_question = question_list[index]
     context = {
         'question': current_question,
         'answers': current_question.answers.all(),
-        'progress': f"{index + 1} / {total_questions}"
+        'progress': f"{index + 1} / {total_questions}",
+        'execution': execution,
     }
 
     return render(request, 'question.html', context)
+
+
+def test_completed_view(request, revision_code):
+    execution = get_object_or_404(TestExecution, revision_code=revision_code)
+    given_answers = execution.given_answers_through.select_related('answer__question')
+
+    context = {
+        'execution': execution,
+        'given_answers': given_answers,
+    }
+
+    return render(request, 'test_completed.html', context)
 
 
 def submit_answer(request, test_id):
